ca55e1eb2d03b1d3d2e12d0b07b9bff21f5373a0
İlhan Cüvelek <75850897+ilhancuvelek@users.noreply.github.com>
2021-03-11 00:09:09 +0300

    MyFinalProject

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -40,6 +40,8 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
+
+            services.AddCors();
             
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -71,6 +73,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
             app.UseRouting();
